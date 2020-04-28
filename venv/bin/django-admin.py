b9901f4e6cf9729c609deb369f1d8dad5f0c93c9
Maxvdburg <maxvanderburg@live.nl>
2020-04-28 15:38:55 +0200

    Upload files

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Dominique/Downloads/CloneApp/nameless-lowlands-57172/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
