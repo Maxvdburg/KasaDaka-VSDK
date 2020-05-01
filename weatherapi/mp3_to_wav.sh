0f5c067316d99c1f9dc0a43c317ceb3850448aa3
Max van der Burg <maxvanderburg@live.nl>
2020-05-01 11:33:59 +0200

    TEST create database file and python file for each city

--- weatherapi/mp3_to_wav.sh
+++ weatherapi/mp3_to_wav.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+if [ ! -d converted/gao ]
+then
+ mkdir converted/gao;
+fi;
+
 for i in ./*.mp3
 do
     sox "$i" "$(basename "$i" .mp3).wav"
+done
+
+for i in *.wav
+ do sox -S "$i" -r 8k -b 16 -c 1 -e signed-integer "converted/gao/$i";
 done
